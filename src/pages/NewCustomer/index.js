00b415cd3dad9db5faf5e138ee986e3eb17cfffd
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2021-11-24 18:54:20 -0300

    Adiciona biblioteca Paper

--- src/pages/NewCustomer/index.js
+++ src/pages/NewCustomer/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
 import NavBar from '../../components/NavBar';
-import {Container} from './style';
+import {Component, Container, FormBox} from './style';
+import {TextInput} from 'react-native-paper';
 
 const NewCustomer = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
   return (
     <Container>
-      <Text>Novo Cliente</Text>
+      <FormBox>
+        <Component>
+          <TextInput
+            mode="outlined"
+            label="Nome"
+            value={name}
+            onChangeText={name => setName(name)}
+          />
+        </Component>
+
+        <TextInput
+          mode="outlined"
+          label="Valor"
+          value={price}
+          onChangeText={price => setPrice(price)}
+        />
+      </FormBox>
+
       <NavBar add={true} />
     </Container>
   );
--- src/pages/NewCustomer/style.js
+++ src/pages/NewCustomer/style.js
@@ -4,3 +4,9 @@ export const Container = styled.View`
   flex: 1;
   justify-content: space-between;
 `;
+export const FormBox = styled.View`
+  padding: 15px;
+`;
+export const Component = styled.View`
+  margin-bottom: 10px;
+`;
